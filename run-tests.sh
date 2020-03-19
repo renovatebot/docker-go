9e9ceef4a688bb26ca4cc13cbc6db711e66c6209
Michael Kriese <michael.kriese@visualon.de>
2020-03-19 21:20:44 +0100

    chore exit on error

--- run-tests.sh
+++ run-tests.sh
@@ -4,5 +4,5 @@ find ./tests -maxdepth 1 -type f -name '*.sh' -print0 | while read -d $'\0' file
 do
     echo "Runnning test: ${file##*/}"
     bash $file
-    echo $?
+    [ "$?" = "0" ] || exit 1
 done
