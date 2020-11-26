be6ffddd206139b65c192543ad1692b55a2c2caf
Michael Kriese <michael.kriese@visualon.de>
2020-11-26 12:44:36 +0100

    feat: support flavor

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 
 find ./tests -maxdepth 1 -type f -name '*.sh' -print0 | while read -d $'\0' file
 do
-    echo "Runnning test: ${file##*/}"
-    bash $file
-    [ "$?" = "0" ] || exit 1
+  echo "Runnning test: ${file##*/}"
+  bash $file
+  [ "$?" = "0" ] || exit 1
 done
--- tests/init.sh
+++ tests/init.sh
@@ -2,12 +2,12 @@
 set -e
 
 init_fixtures () {
-    rm -rf ./.tmp/
-    mkdir -p ./.tmp/
-    cp -R ./tests/__fixtures__/* ./.tmp/
-    chmod -R 777 ./.tmp/
+  rm -rf ./.tmp/
+  mkdir -p ./.tmp/
+  cp -R ./tests/__fixtures__/* ./.tmp/
+  chmod -R 777 ./.tmp/
 }
 
 run_test(){
-    docker run --rm -t -v $(pwd)/.tmp/$1:/src -w /src ${DOCKER_REPO} go ${@:2}
+  docker run --rm -t -v $(pwd)/.tmp/$1:/src -w /src ${DOCKER_REPO} go ${@:2}
 }
