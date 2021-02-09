520474957f6b374757b2ae8892adf15b91a35434
Michael Kriese <michael.kriese@visualon.de>
2021-02-09 15:34:13 +0100

    fix: cleanup tests, already in buildpack

--- run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find ./tests -maxdepth 1 -type f -name '*.sh' -print0 | while read -d $'\0' file
-do
-  echo "Runnning test: ${file##*/}"
-  bash $file
-  [ "$?" = "0" ] || exit 1
-done
